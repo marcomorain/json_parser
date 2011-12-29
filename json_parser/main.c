0a9bfe390cf0f5848733ed2768212d5922ccdd24
Marc O'Morain <github.com@marcomorain.com>
2011-12-29 11:09:43 +0000

    Add some tests, start to make the parser event driven

--- json_parser/main.c
+++ json_parser/main.c
@@ -1,4 +1,43 @@
 #include "../src/json.h"
+#include <stdio.h>
+
+static void do_file(const char* file_name)
+{
+    FILE* file = fopen(file_name, "r");
+    
+
+    char * buffer;
+    size_t result;
+    
+
+    if (file == NULL)
+    {
+        fprintf(stderr, "File error %s", file_name);
+        return;
+    }
+    
+    // obtain file size:
+    fseek(file , 0 , SEEK_END);
+    size_t length = ftell(file);
+    rewind(file);
+    
+    // allocate memory to contain the whole file:
+    char* buffer = malloc(length);
+    
+    if (buffer == NULL) {fputs ("Memory error",stderr); exit (2);}
+    
+    // copy the file into the buffer:
+    result = fread (buffer,1,lSize,pFile);
+    if (result != lSize) {fputs ("Reading error",stderr); exit (3);}
+    
+    /* the whole file is now loaded in the memory buffer. */
+    
+    // terminate
+    fclose (pFile);
+    free (buffer);
+    return 0;
+    
+}
 
 int main (int argc, const char * argv[])
 {
--- src/json.h
+++ src/json.h
@@ -1,8 +1,14 @@
 #ifndef JSON_H
 #define JSON_H
 
+struct json_callbacks {
+    void (*on_null)();
+    void (*on_boolean)(int value);
+    void (*on_number)(double value);
+};
+
 struct json_value;
-struct json_value* json_parse(const char* source);
+struct json_value* json_parse(const char* source, struct json_callbacks* callbacks);
 void json_destroy(struct json_value* value);
 
 #endif
